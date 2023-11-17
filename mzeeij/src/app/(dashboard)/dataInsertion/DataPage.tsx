8b473e28d20dd809f72b34b903bc19595897017a
QAGatPurdue <qghabrah@purdue.edu>
2023-11-17 19:11:47 +0300

    fix DataPage #2

--- mzeeij/src/app/(dashboard)/dataInsertion/DataPage.tsx
+++ mzeeij/src/app/(dashboard)/dataInsertion/DataPage.tsx
@@ -26,7 +26,8 @@ export function DataPage() {
             <CardHeader>
               <CardTitle>Account</CardTitle>
               <CardDescription>
-                Make changes to your account here. Click save when you're done.
+                Make changes to your account here. Click save when you&apos;re
+                done.
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
@@ -49,7 +50,8 @@ export function DataPage() {
             <CardHeader>
               <CardTitle>Password</CardTitle>
               <CardDescription>
-                Change your password here. After saving, you'll be logged out.
+                Change your password here. After saving, you&apos;ll be logged
+                out.
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
