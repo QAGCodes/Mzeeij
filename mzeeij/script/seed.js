c82595fc1916bf834479a875159bf440134c7b19
alialfar7 <143697708+alialfar7@users.noreply.github.com>
2023-11-20 18:06:16 -0600

    I automated creating tables and seeding fake data

--- mzeeij/script/seed.js
+++ mzeeij/script/seed.js
@@ -1,173 +1,70 @@
 const { db } = require('@vercel/postgres');
-const {
-  invoices,
-  customers,
-  revenue,
-  users,
-} = require('../src/lib/placeholder-data.js');
-const bcrypt = require('bcrypt');
+const pgp = require('pg-promise')();
+const fs = require('fs');
+const path = require('../src/lib/inventory.sql');
+const faker = require('faker');
+const getPlaceholderData = require('../src/lib/placeholder-data');
 
-async function seedUsers(client) {
+// Function to read the SQL file
+function readSqlFile(file) {
+  const fullPath = path.join(__dirname, file);
+  return new Promise((resolve, reject) => {
+    fs.readFile(fullPath, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+}
+
+// Function to execute the SQL file
+async function executeSqlFile(db, file) {
+  const data = await readSqlFile(file);
+  return db.none(data);
+}
+
+// Function to seed the database
+async function seedDatabase() {
+
   try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Create the "users" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS users (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
-      );
-    `;
+    await executeSqlFile(db, 'inventory.sql');
+    console.log('Database seeded!');
+  } catch (err) {
+    console.error('Error seeding database', err);
+  } finally {
+    pgp.end();
+  }
+}
 
-    console.log(`Created "users" table`);
+async function seedfakeData() {
+  const data = getPlaceholderData();
+  const client = await db.connect();
 
-    // Insert data into the "users" table
-    const insertedUsers = await Promise.all(
-      users.map(async (user) => {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        return client.sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `;
+  try {
+    await client.query('BEGIN');
+    await Promise.all(
+      Object.keys(data).map((table) => {
+        const rows = data[table];
+        const columns = Object.keys(rows[0]);
+        const query = pgp.helpers.insert(rows, columns, table);
+        return client.query(query);
       }),
     );
-
-    console.log(`Seeded ${insertedUsers.length} users`);
-
-    return {
-      createTable,
-      users: insertedUsers,
-    };
-  } catch (error) {
-    console.error('Error seeding users:', error);
-    throw error;
-  }
-}
-
-async function seedInvoices(client) {
-  try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-    // Create the "invoices" table if it doesn't exist
-    const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS invoices (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    customer_id UUID NOT NULL,
-    amount INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    date DATE NOT NULL
-  );
-`;
-
-    console.log(`Created "invoices" table`);
-
-    // Insert data into the "invoices" table
-    const insertedInvoices = await Promise.all(
-      invoices.map(
-        (invoice) => client.sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedInvoices.length} invoices`);
-
-    return {
-      createTable,
-      invoices: insertedInvoices,
-    };
-  } catch (error) {
-    console.error('Error seeding invoices:', error);
-    throw error;
-  }
-}
-
-async function seedCustomers(client) {
-  try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-    // Create the "customers" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS customers (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        image_url VARCHAR(255) NOT NULL
-      );
-    `;
-
-    console.log(`Created "customers" table`);
-
-    // Insert data into the "customers" table
-    const insertedCustomers = await Promise.all(
-      customers.map(
-        (customer) => client.sql`
-        INSERT INTO customers (id, name, email, image_url)
-        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedCustomers.length} customers`);
-
-    return {
-      createTable,
-      customers: insertedCustomers,
-    };
-  } catch (error) {
-    console.error('Error seeding customers:', error);
-    throw error;
-  }
-}
-
-async function seedRevenue(client) {
-  try {
-    // Create the "revenue" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS revenue (
-        month VARCHAR(4) NOT NULL UNIQUE,
-        revenue INT NOT NULL
-      );
-    `;
-
-    console.log(`Created "revenue" table`);
-
-    // Insert data into the "revenue" table
-    const insertedRevenue = await Promise.all(
-      revenue.map(
-        (rev) => client.sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
-      `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedRevenue.length} revenue`);
-
-    return {
-      createTable,
-      revenue: insertedRevenue,
-    };
-  } catch (error) {
-    console.error('Error seeding revenue:', error);
-    throw error;
+    await client.query('COMMIT');
+    console.log('Fake data seeded!');
+  } catch (err) {
+    await client.query('ROLLBACK');
+    console.error('Error seeding fake data', err);
+  } finally {
+    client.release();
+    pgp.end();
   }
 }
 
 async function main() {
   const client = await db.connect();
 
-  await seedUsers(client);
-  await seedCustomers(client);
-  await seedInvoices(client);
-  await seedRevenue(client);
-
+  await seedDatabase(client);
+  await seedfakeData(client);
   await client.end();
 }
 
---
+++ mzeeij/src/lib/inventory.sql
@@ -0,0 +1,89 @@
+CREATE TABLE user (
+  id SERIAL PRIMARY KEY,
+  roleId INTEGER NOT NULL,
+  firstName TEXT,
+  lastName TEXT,
+  username TEXT UNIQUE,
+  mobile TEXT UNIQUE,
+  email TEXT UNIQUE,
+  passwordHash TEXT NOT NULL,
+  registeredAt TIMESTAMP NOT NULL,
+  FOREIGN KEY (roleId) REFERENCES role (id)
+);
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE supplier (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  contact TEXT,
+  address TEXT
+);
+
+CREATE TABLE product (
+  id SERIAL PRIMARY KEY,
+  supplierId INTEGER NOT NULL,
+  orderId INTEGER,
+  title TEXT NOT NULL,
+  summary TEXT,
+  type INTEGER NOT NULL DEFAULT 0,
+  sku TEXT NOT NULL,
+  createdAt TIMESTAMP NOT NULL,
+  updatedAt TIMESTAMP,
+  content TEXT,
+  FOREIGN KEY (supplierId) REFERENCES supplier (id),
+  FOREIGN KEY (orderId) REFERENCES orders (id)
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  userId INTEGER NOT NULL,
+  type INTEGER NOT NULL DEFAULT 0,
+  status INTEGER NOT NULL DEFAULT 0,
+  subTotal REAL NOT NULL DEFAULT 0,
+  MetaProductDiscount REAL NOT NULL DEFAULT 0,
+  tax REAL NOT NULL DEFAULT 0,
+  shipping REAL NOT NULL DEFAULT 0,
+  total REAL NOT NULL DEFAULT 0,
+  promo TEXT,
+  discount REAL NOT NULL DEFAULT 0,
+  grandTotal REAL NOT NULL DEFAULT 0,
+  createdAt TIMESTAMP NOT NULL,
+  FOREIGN KEY (userId) REFERENCES user (id)
+);
+
+CREATE TABLE inventory (
+  id SERIAL PRIMARY KEY,
+  productId INTEGER NOT NULL,
+  quantity INTEGER NOT NULL,
+  FOREIGN KEY (productId) REFERENCES product (id)
+);
+
+CREATE TABLE orderItem (
+  id SERIAL PRIMARY KEY,
+  orderId INTEGER NOT NULL,
+  productId INTEGER NOT NULL,
+  quantity INTEGER NOT NULL,
+  FOREIGN KEY (orderId) REFERENCES orders (id),
+  FOREIGN KEY (productId) REFERENCES product (id)
+);
+
+CREATE TABLE category (
+  id SERIAL PRIMARY KEY,
+  productId INTEGER,
+  title TEXT NOT NULL,
+  metaTitle TEXT,
+  slug TEXT NOT NULL,
+  content TEXT,
+  FOREIGN KEY (productId) REFERENCES product (id)
+);
+
+CREATE TABLE admin (
+  id SERIAL PRIMARY KEY,
+  userId INTEGER NOT NULL,
+  accessLevel INTEGER NOT NULL,
+  FOREIGN KEY (userId) REFERENCES user (id)
+);
