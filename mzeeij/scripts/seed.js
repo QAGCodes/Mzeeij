c1727a347452d790e842619a3bb3995765efa206
alialfar7 <143697708+alialfar7@users.noreply.github.com>
2024-01-10 10:47:53 +0300

    edited seed.js for data insertion

--- mzeeij/scripts/seed.js
+++ mzeeij/scripts/seed.js
@@ -2,142 +2,23 @@ const { db } = require("@vercel/postgres");
 const data = require("@/lib/seedData")();
 const bcrypt = require("bcrypt");
 
-async function createTables(client: any) {
-  try {
-    await client.sql`drop table if exists role, users, supplier, orders, product, inventory, orderItem, category, admin cascade`;
 
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Create the "users" table if it doesn't exist
-    const roleTable = await client.sql`
-CREATE TABLE role (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
-);
-    `;
-    const usersTable = await client.sql`
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  roleId INTEGER NOT NULL,
-  firstName TEXT,
-  lastName TEXT,
-  username TEXT UNIQUE,
-  mobile TEXT UNIQUE,
-  email TEXT UNIQUE,
-  passwordHash TEXT NOT NULL,
-  registeredAt TIMESTAMP NOT NULL,
-  FOREIGN KEY (roleId) REFERENCES role (id)
-);
-    `;
-
-    const supplierTable = await client.sql`
-      CREATE TABLE supplier (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  contact TEXT,
-  address TEXT
-      );
-    `;
-
-    const ordersTable = await client.sql`
-CREATE TABLE orders (
-  id SERIAL PRIMARY KEY,
-  userId INTEGER NOT NULL,
-  type INTEGER NOT NULL DEFAULT 0,
-  status INTEGER NOT NULL DEFAULT 0,
-  subTotal REAL NOT NULL DEFAULT 0,
-  MetaProductDiscount REAL NOT NULL DEFAULT 0,
-  tax REAL NOT NULL DEFAULT 0,
-  shipping REAL NOT NULL DEFAULT 0,
-  total REAL NOT NULL DEFAULT 0,
-  promo TEXT,
-  discount REAL NOT NULL DEFAULT 0,
-  grandTotal REAL NOT NULL DEFAULT 0,
-  createdAt TIMESTAMP NOT NULL,
-  FOREIGN KEY (userId) REFERENCES users (id)
-);
-    `;
-
-    const productTable = await client.sql`
-CREATE TABLE product (
-  id SERIAL PRIMARY KEY,
-  supplierId INTEGER NOT NULL,
-  orderId INTEGER,
-  title TEXT NOT NULL,
-  summary TEXT,
-  type INTEGER NOT NULL DEFAULT 0,
-  sku TEXT NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  updatedAt TIMESTAMP,
-  content TEXT,
-  FOREIGN KEY (supplierId) REFERENCES supplier (id),
-  FOREIGN KEY (orderId) REFERENCES orders (id)
-);
-    `;
-
-    const inventoryTable = await client.sql`
-CREATE TABLE inventory (
-  id SERIAL PRIMARY KEY,
-  productId INTEGER NOT NULL,
-  quantity INTEGER NOT NULL,
-  FOREIGN KEY (productId) REFERENCES product (id)
-);
-    `;
-
-    const orderItemTable = await client.sql`
-CREATE TABLE orderItem (
-  id SERIAL PRIMARY KEY,
-  orderId INTEGER NOT NULL,
-  productId INTEGER NOT NULL,
-  quantity INTEGER NOT NULL,
-  FOREIGN KEY (orderId) REFERENCES orders (id),
-  FOREIGN KEY (productId) REFERENCES product (id)
-);
-    `;
-
-    const categoryTable = await client.sql`
-CREATE TABLE category (
-  id SERIAL PRIMARY KEY,
-  productId INTEGER,
-  title TEXT NOT NULL,
-  metaTitle TEXT,
-  slug TEXT NOT NULL,
-  content TEXT,
-  FOREIGN KEY (productId) REFERENCES product (id)
-);
-    `;
-
-    const adminTable = await client.sql`
-CREATE TABLE admin (
-  id SERIAL PRIMARY KEY,
-  userId INTEGER NOT NULL,
-  accessLevel INTEGER NOT NULL,
-  FOREIGN KEY (userId) REFERENCES users (id)
-);
-    `;
-
-    console.log(`Created "users" table`);
-  } catch (error) {
-    console.error("Error creating tables:", error);
-    throw error;
-  }
-}
-
-async function seedRole(client: any) {
+async function seedCompany(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Insert data into the "role" table
-    const insertedRole = await Promise.all(
-      data.role.map(
-        (role: any) => client.sql`
-            INSERT INTO role (name)
-            VALUES (${role.name});
-        `
+    // Insert data into the "company" table
+    const insertedCompany = await Promise.all(
+      data.company.map(
+        (company) => client.sql`
+                INSERT INTO company (CompanyName, CRnum, registeredat)
+                VALUES (${company.CompanyName}, ${company.CRnum}, ${company.registeredat});
+            `
       )
     );
 
-    console.log(`Seeded ${insertedRole.length} role`);
+    console.log(`Seeded ${insertedCompany.length} companies`);
   } catch (error) {
-    console.error("Error seeding role:", error);
+    console.error("Error seeding companies:", error);
     throw error;
   }
 }
@@ -149,8 +30,8 @@ async function seedUsers(client: any) {
     const insertedUsers = await Promise.all(
       data.users.map(
         (user: any) => client.sql`
-                INSERT INTO users (roleId, firstName, lastName, username, mobile, email, passwordHash, registeredAt)
-                VALUES (${user.roleId}, ${user.firstName}, ${user.lastName}, ${user.username}, ${user.mobile}, ${user.email}, ${user.passwordHash}, ${user.registeredAt});
+                INSERT INTO users (CompanyName, firstname, lastname, username, mobile, email, passwordhash, registeredat, role)
+                VALUES (${user.CompanyName}, ${user.firstname}, ${user.lastname}, ${user.username}, ${user.mobile}, ${user.email}, ${user.passwordhash}, ${user.registeredat}, ${user.role});
             `
       )
     );
@@ -169,8 +50,8 @@ async function seedSupplier(client: any) {
     const insertedSupplier = await Promise.all(
       data.supplier.map(
         (supplier: any) => client.sql`
-                INSERT INTO supplier (name, contact, address)
-                VALUES (${supplier.name}, ${supplier.contact}, ${supplier.address});
+                INSERT INTO supplier (name, phonenumber, address, email)
+                VALUES (${supplier.name}, ${supplier.phonenumber}, ${supplier.address}, ${supplier.email});
             `
       )
     );
@@ -182,6 +63,26 @@ async function seedSupplier(client: any) {
   }
 }
 
+async function seedSupplierCompany(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Insert data into the "SupplierCompany" table
+    const insertedSupplierCompany = await Promise.all(
+      data.supplierCompanies.map(
+        (supplierCompany) => client.sql`
+                INSERT INTO SupplierCompany (supplierId, companyName)
+                VALUES (${supplierCompany.supplierId}, ${supplierCompany.companyName});
+            `
+      )
+    );
+
+    console.log(`Seeded ${insertedSupplierCompany.length} supplier companies`);
+  } catch (error) {
+    console.error("Error seeding supplier companies:", error);
+    throw error;
+  }
+}
+
 async function seedOrders(client: any) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -189,8 +90,8 @@ async function seedOrders(client: any) {
     const insertedOrders = await Promise.all(
       data.orders.map(
         (order: any) => client.sql`
-                    INSERT INTO orders (userId, type, status, subTotal, MetaProductDiscount, tax, shipping, total, promo, discount, grandTotal, createdAt)
-                    VALUES (${order.userId}, ${order.type}, ${order.status}, ${order.subTotal}, ${order.MetaProductDiscount}, ${order.tax}, ${order.shipping}, ${order.total}, ${order.promo}, ${order.discount}, ${order.grandTotal}, ${order.createdAt});
+                    INSERT INTO orders (CompanyName, userid, type, status, createdat)
+                    VALUES (${order.CompanyName}, ${order.userid}, ${order.type}, ${order.status}, ${order.createdat});
                 `
       )
     );
@@ -202,102 +103,62 @@ async function seedOrders(client: any) {
   }
 }
 
-async function seedProduct(client: any) {
+async function seedMetaProduct(client: any) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Insert data into the "product" table
-    const insertedProduct = await Promise.all(
-      data.product.map(
-        (product: any) => client.sql`
-                    INSERT INTO product (supplierId, orderId, title, summary, type, sku, createdAt, updatedAt, content)
-                    VALUES (${product.supplierId}, ${product.orderId}, ${product.title}, ${product.summary}, ${product.type}, ${product.sku}, ${product.createdAt}, ${product.updatedAt}, ${product.content});
+    // Insert data into the "Meta_product" table
+    const insertedMetaProduct = await Promise.all(
+      data.Meta_product.map(
+        (metaProduct: any) => client.sql`
+                    INSERT INTO Meta_product (CompanyName, supplierid, title, summary, sku, upc, price, createdat, updatedat, estimatedExp, StockStatus)
+                    VALUES (${metaProduct.CompanyName}, ${metaProduct.supplierid}, ${metaProduct.title}, ${metaProduct.summary}, ${metaProduct.sku}, ${metaProduct.upc}, ${metaProduct.price}, ${metaProduct.createdat}, ${metaProduct.updatedat}, ${metaProduct.estimatedExp}, ${metaProduct.StockStatus});
                 `
       )
     );
 
-    console.log(`Seeded ${insertedProduct.length} product`);
+    console.log(`Seeded ${insertedMetaProduct.length} Meta_product`);
   } catch (error) {
-    console.error("Error seeding product:", error);
+    console.error("Error seeding Meta_product:", error);
     throw error;
   }
 }
 
-async function seedInventory(client: any) {
+async function seedItem(client: any) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Insert data into the "inventory" table
-    const insertedInventory = await Promise.all(
-      data.inventory.map(
-        (inventory: any) => client.sql`
-                    INSERT INTO inventory (productId, quantity)
-                    VALUES (${inventory.productId}, ${inventory.quantity});
+    // Insert data into the "item" table
+    const insertedItem = await Promise.all(
+      data.item.map(
+        (item: any) => client.sql`
+                    INSERT INTO item (Metaid, orderid, insertedat, removedat, location, Expiredate)
+                    VALUES (${item.Metaid}, ${item.orderid}, ${item.insertedat}, ${item.removedat}, ${item.location}, ${item.Expiredate});
                 `
       )
     );
 
-    console.log(`Seeded ${insertedInventory.length} inventory`);
+    console.log(`Seeded ${insertedItem.length} item`);
   } catch (error) {
-    console.error("Error seeding inventory:", error);
+    console.error("Error seeding item:", error);
     throw error;
   }
 }
 
-async function seedOrderItem(client: any) {
+async function seedInvoice(client: any) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Insert data into the "orderItem" table
-    const insertedOrderItem = await Promise.all(
-      data.orderItem.map(
-        (orderItem: any) => client.sql`
-                    INSERT INTO orderItem (orderId, productId, quantity)
-                    VALUES (${orderItem.orderId}, ${orderItem.productId}, ${orderItem.quantity});
+    // Insert data into the "invoice" table
+    const insertedInvoice = await Promise.all(
+      data.invoice.map(
+        (invoice: any) => client.sql`
+                    INSERT INTO invoice (orderid, subtotal, metaproductdiscount, tax, shipping, total, promo, discount, grandtotal, createdat)
+                    VALUES (${invoice.orderid}, ${invoice.subtotal}, ${invoice.metaproductdiscount}, ${invoice.tax}, ${invoice.shipping}, ${invoice.total}, ${invoice.promo}, ${invoice.discount}, ${invoice.grandtotal}, ${invoice.createdat});
                 `
       )
     );
 
-    console.log(`Seeded ${insertedOrderItem.length} orderItem`);
+    console.log(`Seeded ${insertedInvoice.length} invoice`);
   } catch (error) {
-    console.error("Error seeding orderItem:", error);
-    throw error;
-  }
-}
-
-async function seedCategory(client: any) {
-  try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Insert data into the "category" table
-    const insertedCategory = await Promise.all(
-      data.category.map(
-        (category: any) => client.sql`
-                    INSERT INTO category (productId, title, metaTitle, slug, content)
-                    VALUES (${category.productId}, ${category.title}, ${category.metaTitle}, ${category.slug}, ${category.content});
-                `
-      )
-    );
-
-    console.log(`Seeded ${insertedCategory.length} category`);
-  } catch (error) {
-    console.error("Error seeding category:", error);
-    throw error;
-  }
-}
-
-async function seedAdmin(client: any) {
-  try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Insert data into the "admin" table
-    const insertedAdmin = await Promise.all(
-      data.admin.map(
-        (admin: any) => client.sql`
-                    INSERT INTO admin (userId, accessLevel)
-                    VALUES (${admin.userId}, ${admin.accessLevel});
-                `
-      )
-    );
-
-    console.log(`Seeded ${insertedAdmin.length} admin`);
-  } catch (error) {
-    console.error("Error seeding admin:", error);
+    console.error("Error seeding invoice:", error);
     throw error;
   }
 }
@@ -305,16 +166,14 @@ async function seedAdmin(client: any) {
 async function main() {
   const client = await db.connect();
 
-  await createTables(client);
-  await seedRole(client);
   await seedUsers(client);
   await seedSupplier(client);
   await seedOrders(client);
-  await seedProduct(client);
-  await seedInventory(client);
-  await seedOrderItem(client);
-  await seedCategory(client);
-  await seedAdmin(client);
+  await seedMetaProduct(client);
+  await seedItem(client);
+  await seedInvoice(client);
+  await seedCompany(client);
+  await seedSupplierCompany(client);
 
   await client.end();
 }
