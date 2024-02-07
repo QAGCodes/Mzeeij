8adef25d6fd3fb2ad44825cc0a59a9fbce81645a
Qusai Ghabrah <qghabrah@purdue.edu>
2024-02-07 22:37:59 +0300

    Color Changes

--- mzeeij/src/app/login/page.tsx
+++ mzeeij/src/app/login/page.tsx
@@ -13,30 +13,15 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-import React, {
-  useState,
-  ChangeEvent,
-  MouseEventHandler,
-  useEffect,
-} from "react";
+import React, { useState, ChangeEvent } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import One8Logo from "../../../public/One8_logo.svg";
-
+import One8Logo from "../../../public/logo/original/One8_logo.svg";
 
 import { useRouter } from "next/navigation";
 
-
 const Login = () => {
   const router = useRouter();
-
-  // const { data: session } = useSession();
-
-  // useEffect(() => {
-  //   if (session?.user) {
-  //     router.push("/dashboard/analytics");
-  //   }
-  // }, [session]);
 
   const [formData, setFormData] = useState({
     username: "",
@@ -51,28 +36,11 @@ const Login = () => {
 
   const handleSubmit = async () => {
     router.push("/dashboard/analytics");
-    // const { username, password } = formData;
-    // try {
-    //   const result = await signIn("credentials", {
-    //     ...formData,
-    //     redirect: false,
-    //   }).then((callback) => {
-    //     console.log("hi", callback);
-    //     if (callback?.ok && callback?.status === 200) {
-    //       alert("success signing in");
-    //       router.push("/dashboard/analytics");
-    //     } else {
-    //       alert("error signing in");
-    //     }
-    //   });
-    // } catch (error) {
-    //   console.log(error);
-    // }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Card className="bg-white">
+    <div className="flex flex-col items-center justify-center h-screen w-screen red-outline">
+      <Card className="">
         <CardHeader className="text-center p-4">
           <CardTitle>
             <Image src={One8Logo} width={100} height={100} alt="hi"></Image>
@@ -87,7 +55,7 @@ const Login = () => {
                 <Input
                   id="username"
                   placeholder="Your username"
-                  onChange={handleChange}
+                  onInput={handleChange}
                 />
               </div>
               <div className="flex flex-col space-y-1.5">
@@ -95,7 +63,7 @@ const Login = () => {
                 <Input
                   id="password"
                   placeholder="Your Password"
-                  onChange={handleChange}
+                  onInput={handleChange}
                   type="password"
                 />
               </div>
--- mzeeij/src/app/page.tsx
+++ mzeeij/src/app/page.tsx
@@ -1,18 +1,19 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-screen">
-      <Card className="bg-white flex flex-col justify-center items-center w-[20rem] h-[20rem]">
-        <Image src={"/One8_logo.svg"} width={200} height={200} alt="hi"></Image>
-        <Link href={"/login"} className="bg-slate-400 rounded p-2">
-          Login
-        </Link>
-      </Card>
+    <div className="flex flex-col justify-center items-center h-screen w-screen gap-10">
+      <Image
+        src={"logo/nolines-cropped/logo.svg"}
+        width={500}
+        height={500}
+        alt="One8 Logo"
+      ></Image>
+      <Button className="bg-mzeeij-green text-white rounded w-[6rem] h-[3rem] hover:bg-mzeeij-green/70">
+        <Link href={"/login"}>Login</Link>
+      </Button>
     </div>
   );
 }
